a8ace81017f4854f6e4ef42a479a1d314b9d6279
Jaehun-Kim22 <kjaehun22@gmail.com>
2021-05-09 00:55:10 +0900

    [#60] assignment 5 lstm modification complete
    [#60] code refactorization
    
    [#60] lstm zero input added and code refactored for tensorboard flexibility
    
    [#60] lstm output dim changed
    
    [#60] assignment complete

--- tensorflow_project/lstm_mnist.py
+++ tensorflow_project/lstm_mnist.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 import conv_lstm
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 def load_moving_mnist():
@@ -69,28 +69,41 @@ def predictor_model(previous_seq, previous_num, prediction_num):
     unit2 = conv_lstm.Unit(shape=[feature_height, feature_width],
                            kernel_size=[5, 5], filters=64)
 
-    for seq_i in range(previous_num + prediction_num - 1):
+    predictions = []
+    for seq_i in range(previous_num + prediction_num):
+        if seq_i > previous_num - 1:
+            inputs = tf.zeros_like(enc_feature[:, 0, :, :, :])
+        else:
+            inputs = enc_feature[:, seq_i, :, :, :]
         with tf.variable_scope('convlstm_1', reuse=tf.AUTO_REUSE):
-            conv_lstm_h1, conv_lstm_c1 = unit1(
-                inputs=enc_feature[:, seq_i, :, :, :],
-                h=conv_lstm_h1, c=conv_lstm_c1)
+            conv_lstm_h1, conv_lstm_c1 = unit1(inputs=inputs,
+                                               h=conv_lstm_h1, c=conv_lstm_c1)
         with tf.variable_scope('convlstm_2', reuse=tf.AUTO_REUSE):
             conv_lstm_h2, conv_lstm_c2 = unit2(inputs=conv_lstm_h1,
                                                h=conv_lstm_h2, c=conv_lstm_c2)
+        if seq_i > previous_num - 1:
+            predictions.append(decoder(conv_lstm_h2))
 
-    prediction = decoder(conv_lstm_h2)
+    # prediction = decoder(conv_lstm_h2)
+    # tf.summary.image('input_frame_3', previous_seq[:, :, :, 2:3], 3)
+    # tf.summary.image('input_frame_4', previous_seq[:, :, :, 3:4], 3)
+    # tf.summary.image('input_frame_5', previous_seq[:, :, :, 4:5], 3)
+    # tf.summary.image('predicted_next_frame', prediction[:, :, :, :], 3)
 
-    tf.summary.image('input_frame_3', previous_seq[:, :, :, 2:3], 3)
-    tf.summary.image('input_frame_4', previous_seq[:, :, :, 3:4], 3)
-    tf.summary.image('input_frame_5', previous_seq[:, :, :, 4:5], 3)
-    tf.summary.image('predicted_next_frame', prediction[:, :, :, :], 3)
+    # return prediction
 
-    return prediction
+    for i in range(previous_num):
+        tf.summary.image('input_frame_' + str(i + 1), previous_seq[:, :, :, i:i+1],
+                         3)
+    for i in range(prediction_num):
+        tf.summary.image('predicted_frame_' + str(i + 1), predictions[i], 3)
+
+    return tf.concat(predictions, -1)
 
 
 def main():
     previous_num = 5
-    prediction_num = 1
+    prediction_num = 3
     batch_size = 128
     lr = 2e-4
     bta1 = .9
@@ -101,26 +114,38 @@ def main():
                                shape=[None, 64, 64,
                                       previous_num + prediction_num])
     previous_seq = batch_seq[:, :, :, 0: previous_num]
-    gt_next_frame = batch_seq[:, :, :, previous_num: previous_num + prediction_num]
-    predicted_next_frame = predictor_model(previous_seq, previous_num,
-                                           prediction_num)
-    tf.summary.image('gt_next_frame', gt_next_frame[:, :, :, 0: 1], 3)
+    gt_next_frames = batch_seq[:, :, :,
+                     previous_num: previous_num + prediction_num]
+    # predicted_next_frame = predictor_model(previous_seq, previous_num,
+    #                                        prediction_num)
+    pred_next_frames = predictor_model(previous_seq, previous_num,
+                                       prediction_num)
+    for i in range(prediction_num):
+        tf.summary.image('gt_next_frame_' + str(i+1),
+                         gt_next_frames[:, :, :, i:i+1], 3)
 
     with tf.name_scope('Loss'):
-        pixel_loss = tf.reduce_mean(tf.square(predicted_next_frame -
-                                              gt_next_frame))
-        tf.summary.scalar('pixel_loss', pixel_loss)
+        # pixel_loss = tf.reduce_mean(tf.square(predicted_next_frame -
+        #                                       gt_next_frame))
+        sum_loss = 0
+        for i in range(prediction_num):
+            sum_loss += tf.reduce_mean(tf.square(pred_next_frames[:, :, :, i]
+                                                 - gt_next_frames[:, :, :, i]))
+        tf.summary.scalar('pixel_loss', sum_loss)
 
     optimizer = tf.train.AdamOptimizer(learning_rate=lr,
                                        beta1=bta1, beta2=bta2,
-                                       epsilon=eps).minimize(pixel_loss)
+                                       epsilon=eps).minimize(sum_loss)
     summ = tf.summary.merge_all()
 
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
         LOGDIR = './moving_mnist_video_tutorial2/'
-        test_writer = tf.summary.FileWriter(LOGDIR + '/Test')
+        version = 0
+        while os.path.isdir(os.path.join(LOGDIR, 'Test_' + str(version))):
+            version += 1
+        test_writer = tf.summary.FileWriter(LOGDIR + 'Test_' + str(version))
 
         data = load_moving_mnist() / 1.
 
@@ -140,16 +165,18 @@ def main():
                                                                + prediction_num]
             sess.run([optimizer], feed_dict={batch_seq: train_batch})
             if itr % 100 == 0:
-                [train_loss] = sess.run([pixel_loss],
+                [train_loss] = sess.run([sum_loss],
                                         feed_dict={batch_seq: train_batch})
                 feed_dict = {batch_seq: test_data[:, :, :, 0: previous_num
                                                               + prediction_num]}
-                [test_loss, s] = sess.run([pixel_loss, summ],
+                [test_loss, s] = sess.run([sum_loss, summ],
                                           feed_dict=feed_dict)
                 print('@ iteration: %i, Training loss = %.6f, Test loss = %.6f'
                       % (itr, train_loss, test_loss))
                 test_writer.add_summary(s, itr)
-                saver.save(sess, os.path.join(LOGDIR, 'model.ckpt'), itr)
+                saver.save(sess, os.path.join(LOGDIR + 'Test_' + str(version),
+                                              'model.ckpt'),
+                           itr)
 
 
 if __name__ == '__main__':
